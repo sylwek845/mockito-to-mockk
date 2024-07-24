a7522c0a3d0282ad05304e7f2d2309d479c2e847
Sylwester <11651955+sylwek845@users.noreply.github.com>
2024-07-24 11:58:12 +0100

    Add config for relaxed

--- src/main/kotlin/configuration/GlobalConfig.kt
+++ src/main/kotlin/configuration/GlobalConfig.kt
@@ -3,4 +3,6 @@ package configuration
 object GlobalConfig {
     var relaxed: Boolean = true
     var removeEq: Boolean = true
+
+    val relaxesStatement get() = if (relaxed) "relaxed = true, " else ""
 }
