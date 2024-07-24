105354fd6df76e90b1ff3eae1fb28fdb18440a73
Sylwester <11651955+sylwek845@users.noreply.github.com>
2024-07-24 12:24:36 +0100

    Add plugin config for toggles

--- src/main/kotlin/plugin/MockkToolWindow.kt
+++ src/main/kotlin/plugin/MockkToolWindow.kt
@@ -8,7 +8,9 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.OnOffButton
 import com.intellij.ui.content.ContentFactory
+import configuration.GlobalConfig
 import logs.LogKeeper
 import migration.MockitoToMockkConverter
 import tools.getSelectedDocumentOrNull
@@ -17,6 +19,7 @@ import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.event.ActionEvent
 import javax.swing.BorderFactory
+import javax.swing.BoxLayout
 import javax.swing.JButton
 import javax.swing.JPanel
 
@@ -40,7 +43,25 @@ internal class MockkToolWindow : ToolWindowFactory, DumbAware {
 
         private fun createControlsPanel(toolWindow: ToolWindow): JPanel {
             val controlsPanel = JPanel(BorderLayout())
+            controlsPanel.layout = BoxLayout(controlsPanel, BoxLayout.Y_AXIS)
+            val togglePanel = JPanel()
+            togglePanel.layout = BoxLayout(togglePanel, BoxLayout.X_AXIS)
+
             val migrateButton = JButton("Convert to Mockk")
+            val relaxedToggle = OnOffButton()
+            relaxedToggle.onText = "Will Set `relaxed = true`"
+            relaxedToggle.offText = "Will NOT Set `relaxed = true`"
+            relaxedToggle.isSelected = true
+            relaxedToggle.addActionListener {
+                GlobalConfig.relaxed = relaxedToggle.isSelected
+            }
+            val removeEQToggle = OnOffButton()
+            removeEQToggle.offText = "Will Not Remove EQs"
+            removeEQToggle.onText = "Will Remove All EQs"
+            removeEQToggle.isSelected = true
+            removeEQToggle.addActionListener {
+                GlobalConfig.removeEq = removeEQToggle.isSelected
+            }
             migrateButton.addActionListener { e: ActionEvent? -> migrateToMockk(toolWindow) }
             consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(toolWindow.project).console
             consoleView.component.size = Dimension(400, 300)
@@ -48,6 +69,9 @@ internal class MockkToolWindow : ToolWindowFactory, DumbAware {
 
             // You can also use layout managers to set the size and position
             controlsPanel.add(consoleView.component, BorderLayout.CENTER)
+            togglePanel.add(relaxedToggle, BorderLayout.AFTER_LAST_LINE)
+            togglePanel.add(removeEQToggle, BorderLayout.AFTER_LAST_LINE)
+            controlsPanel.add(togglePanel)
             controlsPanel.add(migrateButton, BorderLayout.SOUTH)
             return controlsPanel
         }
