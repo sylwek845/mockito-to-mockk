7f8bbea233e42d9690c1f2ca8027422953e58596
Sylwester <11651955+sylwek845@users.noreply.github.com>
2024-07-25 11:04:53 +0100

    Fix AS compatibility and renaming

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 intellij {
-    version.set("2023.2.6")
+    version.set("2024.1.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
